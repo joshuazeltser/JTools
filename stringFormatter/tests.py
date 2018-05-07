6739229fc0985ea7f853561c89bb0a1f18b806c2
Joshua Zeltser <joshua.zeltser@gmail.com>
2018-05-07 19:05:09 +0100

    I have created functionality for basic string formatting and started to add testing for this functionality

---
+++ stringFormatter/tests.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+
+from stringFormatter.formatter import *
+
+
+class FormatterTests(TestCase):
+
+
+    def test_to_upper_case(self):
+        self.assertEquals(to_upper_case("abcdefgh"), "ABCDEFGH")
+
+    def test_to_upper_case_big(self):
+        self.assertEquals(to_upper_case("abcDefghijkLmnopqrstuvWXYz"), "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+    def test_to_lower_case(self):
+        self.assertEquals(to_lower_case("ABCDEFGH"), "abcdefgh")
+
+    def test_to_lower_case_big(self):
+        self.assertEquals(to_lower_case("ABCDEFGHijklMNOPQRStuvwxyZ"), "abcdefghijklmnopqrstuvwxyz")
+
+    def test_word_occurence_count(self):
+        string = "hello my name is joshua and my name is joshua joshua is my name hello what the hell"
+
+        print (word_occurence_count(string))
