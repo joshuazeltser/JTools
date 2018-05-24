183324b31528621d1a58f2386940cd352628b553
Joshua Zeltser <joshua.zeltser@gmail.com>
2018-05-24 16:42:33 +0100

    created a working webpage for the uppercase function which can be submitted many times without issues

--- stringFormatter/urls.py
+++ stringFormatter/urls.py
@@ -6,10 +6,6 @@ app_name = 'stringFormatter'
 urlpatterns = [
         path('', views.index, name='index'),
 
-        path(r'text/', views.text, name='text'),
-        #
-        # path('<int:text_id>/results', views.result, name='result'),
-
         path(r'details/', views.details, name='details'),
 
 ]
--- stringFormatter/views.py
+++ stringFormatter/views.py
@@ -1,25 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from stringFormatter.formatter import to_upper_case
 from .models import Text
-from django.shortcuts import get_object_or_404
-from django.urls import reverse
-from django.core.exceptions import *
 
-# Create your views here.
 
 def index(request):
-    return render(request, 'stringFormatter/details.html')
-
-def details(request):
     if 'q' in request.POST:
-        message = 'You searched for: %r' % request.POST['q']
+        text = Text()
+        text.result = to_upper_case(request.POST['q'])
+        message = text.result
     else:
         message = 'You submitted an empty form.'
-    return render(request, 'stringFormatter/text.html')
 
-def text(request):
+    return render(request, 'stringFormatter/index.html', {'content': message})
 
-    HttpResponse("This is the result of your requested string formatting %s" )
 
-def result(request, text_id):
-    return HttpResponse("This is the result of your requested string formatting %s" % text_id)
+def details(request):
+    return render(request, 'stringFormatter/index.html', {'content': message})
