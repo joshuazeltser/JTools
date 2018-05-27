c0344f6e4a1e9724e6a8fa57b4d1b7bd4bf98d41
Joshua Zeltser <joshua.zeltser@gmail.com>
2018-05-27 12:37:50 +0100

    successfully completed front end for four of the functions

--- stringFormatter/views.py
+++ stringFormatter/views.py
@@ -1,31 +1,29 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from stringFormatter.formatter import to_upper_case
 from .models import Text
 
 
 def index(request):
-
-
     return render(request, 'stringFormatter/index.html')
 
 
 def formatter(request):
-    message = ""
-    original = ""
 
-    if 'one' in request.POST:
-        print('djksakjsadkjsadkjsadhhdask')
+    message = request.POST.get('input1', '')
+    message2 = request.POST.get('input2','')
+    message3 = request.POST.get('input3', '')
+    message4 = request.POST.get('input4', '')
 
+    if 'lower' in request.POST:
+        message2 = Text.to_lower_case(message)
+    elif 'upper' in request.POST:
+        message2 = Text.to_upper_case(message)
+    elif 'word' in request.POST:
+        message4 = Text.word_count(message3)
+    elif 'char' in request.POST:
+        message4 = Text.char_count(message3)
 
+    print(request.POST)
 
-    if 'q' in request.POST:
-        original = request.POST['q']
-        text = Text()
-        text.result = to_upper_case(original)
-        message = text.result
-
-
-
-
-
-    return render(request, 'stringFormatter/formatter.html', {'content': original, 'content2': message})
+    return render(request, 'stringFormatter/formatter.html', {'content': message, 'content2': message2,
+                                                              'content3': message3, 'content4': message4})
