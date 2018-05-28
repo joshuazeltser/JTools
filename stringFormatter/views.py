88669802216b923b95545ffd4a12a47f5adb55f9
Joshua Zeltser <joshua.zeltser@gmail.com>
2018-05-28 12:00:10 +0100

    successfully solved the probelem of prepopulating textareas

--- stringFormatter/views.py
+++ stringFormatter/views.py
@@ -13,6 +13,9 @@ def formatter(request):
     message2 = request.POST.get('input2','')
     message3 = request.POST.get('input3', '')
     message4 = request.POST.get('input4', '')
+    message5 = request.POST.get('input5', '')
+    message6 = request.POST.get('input6', '')
+    message7 = request.POST.get('input7', '')
 
     if 'lower' in request.POST:
         message2 = Text.to_lower_case(message)
@@ -23,8 +26,12 @@ def formatter(request):
         message4 = Text.word_count(message3)
     elif 'char' in request.POST:
         message4 = Text.char_count(message3)
+    elif 'split' in request.POST:
+        message6 = Text.split_string_by(message5, message7)
 
     print(request.POST)
 
     return render(request, 'stringFormatter/formatter.html', {'content': message, 'content2': message2,
-                                                              'content3': message3, 'content4': message4})
+                                                              'content3': message3, 'content4': message4,
+                                                              'content5': message5, 'content6': message6,
+                                                              'content7': message7})
