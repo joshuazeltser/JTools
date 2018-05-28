1e43888c6edb11651a6c6de62a18b5f6ac9deb47
Joshua Zeltser <joshua.zeltser@gmail.com>
2018-05-28 17:42:19 +0100

    added files for heroku deployment

--- JTools/settings.py
+++ JTools/settings.py
@@ -124,3 +124,14 @@ STATICFILES_DIRS = (
 
     '~/JTools/stringFormatter/static/',
 )
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn JTools.wsgi --log-file -
