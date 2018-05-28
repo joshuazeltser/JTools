9a3f8b31c31e28727d425c14a85078ed753b78e0
Joshua Zeltser <joshua.zeltser@gmail.com>
2018-05-28 22:11:18 +0100

    fixed small error in allowed hosts list

--- JTools/settings.py
+++ JTools/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', os.environ['SECRET_KEY'])
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['joshtools.herokuapp.com', '127.0.0.1', 'http://www.jtools.org.uk/', 'http://jtools.org.uk/']
+ALLOWED_HOSTS = ['joshtools.herokuapp.com', '127.0.0.1', 'www.jtools.org.uk/', 'jtools.org.uk/']
 
 
 # Application definition
