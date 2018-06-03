7228fd20095eca98216f8b693d38c1d712cd53e3
Joshua Zeltser <joshua.zeltser@gmail.com>
2018-06-03 21:34:04 +0100

    starting to add ssl cert

--- JTools/urls.py
+++ JTools/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('.well-known/' , include('letsencrypt.urls')),
     path('stringFormatter/', include('stringFormatter.urls')),
     path('admin/', admin.site.urls),
+
 ]
