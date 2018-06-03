2554b7d3a757d271eb513d8315b7b00d703f3abd
Joshua Zeltser <joshua.zeltser@gmail.com>
2018-06-03 21:55:58 +0100

    added url import

--- JTools/urls.py
+++ JTools/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, url
 
 urlpatterns = [
-    path(r'^\.well-known/' , include('letsencrypt.urls')),
+    url(r'^\.well-known/' , include('letsencrypt.urls')),
     path('stringFormatter/', include('stringFormatter.urls')),
     path('admin/', admin.site.urls),
 
